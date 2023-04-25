a8421fb70b6d795d22b227164ecbbbe515d9a5be
Hugo <hpontes9@gmail.com>
2023-04-25 18:06:06 +0100

    Cleared up example file

--- examples/stm32f767zi_example.c
+++ examples/stm32f767zi_example.c
@@ -17,7 +17,8 @@ static void MX_GPIO_Init(void);
 static void MX_I2C2_Init(void);
 static void MX_UART4_Init(void);
 
-/*Function to facilitate debugging through uart2, adapt to your needs */
+/*Function to facilitate debugging through uart2,
+ *don't use in production code since it is not lightweight at all */
 int stm32_printf(const char *format, ...)
 {
     char str[200];
@@ -29,6 +30,9 @@ int stm32_printf(const char *format, ...)
     return n;
 }
 
+/*Function to be provided that writes a register address and writes a user-defined number of bytes
+ * to I2C being used by MPU6050 from user defined memory location
+ * Also return IO Success code as defined in Mpu6050Flex.h*/
 static IOStatus_t Stm32f767zi_I2C_Write(uint8_t Address,uint32_t Size,uint8_t* DataPtr)
 {
 
@@ -50,16 +54,21 @@ static IOStatus_t Stm32f767zi_I2C_Write(uint8_t Address,uint32_t Size,uint8_t* D
 	return Status;
 }
 
+/*Function to be provided that delays execution by user-defined ms */
 static void Stm32f767zi_Delay(uint32_t ms)
 {
 	HAL_Delay(ms);
 }
 
+/*Function to be provided that obtains ms since start of execution */
 static uint32_t Stm32f767zi_GetMs()
 {
 	return HAL_GetTick();
 }
 
+/*Function to be provided that writes a register address and reads a user-defined number of bytes
+ * from I2C being used by MPU6050 into user defined memory location
+ * Also return IO Success code as defined in Mpu6050Flex.h*/
 static IOStatus_t Stm32f767zi_I2C_Read(uint8_t Address,uint32_t Size,uint8_t* DataPtr)
 {
 	IOStatus_t Status = IO_SUCCESS;
@@ -80,23 +89,28 @@ static IOStatus_t Stm32f767zi_I2C_Read(uint8_t Address,uint32_t Size,uint8_t* Da
 
 int main(void)
 {
+	/*STM32 configurations and initializations*/
 	HAL_Init();
 	SystemClock_Config();
 	MX_GPIO_Init();
 	MX_I2C2_Init();
 	MX_UART4_Init();
 
+	/*Declare Mpu6050 instance handle*/
 	Mpu6050Flex_t Mpu6050;
 
+	/*Allocate and create a Mpu6050 instance/object at the location pointed by Mpu6050*/
 	Mpu6050 = Mpu6050Flex_Create();
 
 	stm32_printf("----------Welcome to Mpu6050 Flex----------\n");
 
+	/*Setup abstract functions that Mpu6050Flex uses and are tightly coupled to the underlying hardware*/
 	Mpu6050Flex_SetIOWrite(Mpu6050,Stm32f767zi_I2C_Write);
 	Mpu6050Flex_SetIORead(Mpu6050,Stm32f767zi_I2C_Read);
 	Mpu6050Flex_SetGetMs(Mpu6050,Stm32f767zi_GetMs);
 	Mpu6050Flex_SetDelay(Mpu6050,Stm32f767zi_Delay);
 
+	/*Verify that there is a responsive MPU6050, if not abort this program*/
 	uint8_t ID;
 	ID = Mpu6050Flex_WhoAmI(Mpu6050);
 
@@ -107,7 +121,16 @@ int main(void)
 	else
 	{
 		stm32_printf("ERROR! MPU6050 Not Found!\n");
+		assert(0);
 	}
+
+	/*Default configurations for mpu6050*/
+	stm32_printf("Configuring...\n");
+	Mpu6050Flex_SetComplementaryFilterCoeffs(Mpu6050,0.98, 0.02);
+	Mpu6050Flex_ConfigSampleRateDivider(Mpu6050,0);
+	Mpu6050Flex_ConfigDigitalLowPassFilter(Mpu6050,MPU6050FLEX_DLPF_CFG0);
+	Mpu6050Flex_ConfigGyroFullScaleRange(Mpu6050,MPU6050FLEX_GYRO_FS_SEL_250);
+	Mpu6050Flex_ConfigAccFullScaleRange(Mpu6050,MPU6050FLEX_ACC_FS_SEL_2);
 
 	stm32_printf("Calibrating...\n");
 	Mpu6050Flex_Calibrate(Mpu6050);
@@ -115,12 +138,13 @@ int main(void)
 	Mpu6050Flex_WakeUp(Mpu6050);
 	stm32_printf("Ready!!!\n");
 
+	/*Get and print attitude every 100 ms*/
 	while (1)
 	{
-
 		Mpu6050Flex_EulerAngles_t RetEuler;
 		RetEuler = Mpu6050Flex_GetEuler(Mpu6050);
 
+		/*Since this is called repeatedly, stm32_printf isn't called*/
 		char Msg[50];
 		sprintf(Msg,"Roll: %.2f, Pitch: %.2f, Yaw: %.2f\n",RetEuler.Roll,RetEuler.Pitch,RetEuler.Yaw);
 		HAL_UART_Transmit(&huart4, (uint8_t *)Msg, 38, HAL_MAX_DELAY);
@@ -128,9 +152,12 @@ int main(void)
 		HAL_Delay(100);
 	}
 
+	/*Will never be reached but this is used to destroy Mpu6050 object/instance */
+	Mpu6050Flex_Destroy(Mpu6050);
+
 }
 
-
+/*STM32 functions*/
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
